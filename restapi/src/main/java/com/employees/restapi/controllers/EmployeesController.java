77c6367182dbf68cf7cc876c87f00120dc73ca49
Jonathan M Borges <jonathan.m.borges@gmail.com>
2020-06-24 02:16:25 -0300

    Implementação PARCIAL da WebAPI, mantendo dados em memória.

--- restapi/src/main/java/com/employees/restapi/controllers/EmployeesController.java
+++ restapi/src/main/java/com/employees/restapi/controllers/EmployeesController.java
@@ -7,8 +7,12 @@ import java.util.Collection;
 import com.employees.restapi.models.Employee;
 import com.employees.restapi.services.IEmployeesService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class EmployeesController {
@@ -21,52 +25,35 @@ public class EmployeesController {
     }
 
     @GetMapping(value = "/api/employees")
-    public Collection<Employee> getAll() {
+    public Collection<Employee> listAll() {
         return service.listAll();
     }
 
     // GET: api/Employees/5
     @GetMapping(value = "/api/employees/{id}")
-    public Employee Get(@PathVariable("id") Long id) {
+    public Employee getById(@PathVariable("id") Long id) {
         var employee = service.getById(id);
         if (employee != null)
             return employee;
         return null;
-        //TODO exception
+        // TODO exception
     }
 
-    //// POST: api/Employees
-    // [HttpPost]
-    // public Employee Post([FromBody] Employee employee)
-    // {
-    // service.Add(employee);
-    // return employee;
-    // }
-    //
-    //// PUT: api/Employees/5
-    // [HttpPut("{id}")]
-    // public ActionResult Put(int id, [FromBody] Employee employee)
-    // {
-    // var employeeFinded = service.GetById(id);
-    // if (employeeFinded == null)
-    // return NotFound();
-    //
-    // employee.Id = id;
-    // service.Update(employee);
-    //
-    // return Ok(employee);
-    // }
-    //
-    //// DELETE: api/Employees/5
-    // [HttpDelete("{id}")]
-    // public ActionResult Delete(int id)
-    // {
-    // var employeeFinded = service.GetById(id);
-    // if (employeeFinded == null)
-    // return NotFound();
-    //
-    // service.DeleteById(id);
-    // return Ok();
-    // }
+    // POST: api/Employees
+    @PostMapping("/api/employees")
+    public Employee add(@RequestBody Employee employee) {
+        return service.add(employee);
+    }
+
+    @PutMapping("/api/employees/{id}")
+    public Employee update(@PathVariable Long id, @RequestBody Employee employee) {
+        employee.setId(id);
+        return service.update(employee);
+    }
+
+    @DeleteMapping("/api/employees/{id}")
+    public Employee deleteEmployee(@PathVariable Long id) {
+        return service.deleteById(id);
+    }
 
 }
